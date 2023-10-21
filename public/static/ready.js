6c7acdebeb8a92f2bf2e507e8fee558229a49c40
goddaren <51778712+goddaren@users.noreply.github.com>
2023-10-21 16:06:15 +0800

    去掉检查更新

--- public/static/ready.js
+++ public/static/ready.js
@@ -49,36 +49,36 @@ $('[data-qrcode-text]').each(function (i) {
 	codeDiv.removeAttribute('id');
 });
 
-let CheckUpdate = localStorage.getItem('UpdateTip') || "true";
-if (CheckUpdate === "true")
-	getAPI('/system/update').then(function (response) {
-		if (!response.success) {
-			console.log('检查更新失败！详细信息：');
-			console.log(response);
-		}
-		console.log('检查更新信息：');
-		console.log(response);
-		const data = response.data;
-		if (data.code === 0) {
-			if (data.have_update) {
-				const div = document.createElement('div');
-				div.id = 'CheckUpdate';
-				div.style.margin = '0.3rem 1rem';
-				div.style.display = 'none';
-				div.innerHTML = `Baiduwp-PHP 项目有新的版本：${data.version}（${data.isPreRelease ? '此版本为预发行版本，' : ''}当前版本为${data.now_version}）！请联系站长更新！
-					&nbsp; <a href="${data.page_url}" target="_blank">发行/下载页面</a><div style="float: right;"><a href="javascript:SetUpdateTip(false);">不再提示</a></div>`;
-				document.body.insertAdjacentElement('beforeBegin', div);
-				if (localStorage.getItem('UpdateTip') !== "false")
-					$('#CheckUpdate').show(1500);
-			}
-		} else if (data.code === 2) {
-			console.log('当前版本为预发行版本，不提示更新。');
-		} else if (data.code === 1) {
-			console.log('服务器获取更新失败！详细信息：');
-		} else {
-			console.log('服务器获取更新失败，且错误码不在支持列表中！详细信息：');
-		}
-	});
+// let CheckUpdate = localStorage.getItem('UpdateTip') || "true";
+// if (CheckUpdate === "true")
+// 	getAPI('/system/update').then(function (response) {
+// 		if (!response.success) {
+// 			console.log('检查更新失败！详细信息：');
+// 			console.log(response);
+// 		}
+// 		console.log('检查更新信息：');
+// 		console.log(response);
+// 		const data = response.data;
+// 		if (data.code === 0) {
+// 			if (data.have_update) {
+// 				const div = document.createElement('div');
+// 				div.id = 'CheckUpdate';
+// 				div.style.margin = '0.3rem 1rem';
+// 				div.style.display = 'none';
+// 				div.innerHTML = `Baiduwp-PHP 项目有新的版本：${data.version}（${data.isPreRelease ? '此版本为预发行版本，' : ''}当前版本为${data.now_version}）！请联系站长更新！
+// 					&nbsp; <a href="${data.page_url}" target="_blank">发行/下载页面</a><div style="float: right;"><a href="javascript:SetUpdateTip(false);">不再提示</a></div>`;
+// 				document.body.insertAdjacentElement('beforeBegin', div);
+// 				if (localStorage.getItem('UpdateTip') !== "false")
+// 					$('#CheckUpdate').show(1500);
+// 			}
+// 		} else if (data.code === 2) {
+// 			console.log('当前版本为预发行版本，不提示更新。');
+// 		} else if (data.code === 1) {
+// 			console.log('服务器获取更新失败！详细信息：');
+// 		} else {
+// 			console.log('服务器获取更新失败，且错误码不在支持列表中！详细信息：');
+// 		}
+// 	});
 
 function SetUpdateTip(value) {
 	localStorage.setItem('UpdateTip', `${value}`); // 不知为啥，只能用string类型
